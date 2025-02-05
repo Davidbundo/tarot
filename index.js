b5acc14e65caec452d85361664b5b552eed92d5f
Davidbundo <152003998+Davidbundo@users.noreply.github.com>
2025-02-05 23:42:38 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './src/Home';
+
+function App() {
+  return (
+    <main>
+
+      <Home />
+    </main>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
